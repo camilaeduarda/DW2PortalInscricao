03e82897322b03c245758a3f963a07fadd4bf43e
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2023-03-19 20:19:36 -0400

    added all info of the system

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 
+let oldScreen = null
+
+
 function openShowcase(screen) {
-    document.querySelector('#showcase-container').innerHTML = screen
-    document.querySelectorAll('.show-option').forEach(
-        (e, i) => e.className = i === screen ? 'show-option active' : 'show-option'
+
+    if (oldScreen == screen) {
+        document.querySelector('#showcase-container').innerHTML = ''
+        document.querySelectorAll('.show-option').forEach(
+            (e, i) => e.className = 'show-option'
         )
-    document.querySelector('#showcase-container').style.display = 'block'
+        document.querySelector('#showcase-container').style.display = 'none'
+    } else {
+        document.querySelector('#showcase-container').innerHTML = document.querySelector(`#screen-${screen}`).innerHTML
+        document.querySelectorAll('.show-option').forEach(
+            (e, i) => e.className = i === screen ? 'show-option active' : 'show-option'
+        )
+        document.querySelector('#showcase-container').style.display = 'block'
+    }
+
+    oldScreen = screen
 }
 
-// window.onload = () => openShowcase(1) 
+
+// window.onload = () => openShowcase(3)
