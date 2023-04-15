bbd023476855261f5568094ed59e84ecdccc4b6e
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2023-04-14 23:00:23 -0300

    Armadilha Arco-Íris De Petit Four (1.0)
    Versão Do Projeto: Armadilha Arco-Íris De Petit Four (1.0)
    
     - Division and organization of code files, slight increase in their security and a small pinch of animation in the whole project!

---
+++ DW2 - Candy World/Petit Four (1.0)/js/script.js
@@ -0,0 +1,64 @@
+let oldScreen = null
+
+function openShowcase(screen) {
+  if (oldScreen === screen) {
+    document.querySelector('#showcase-container').innerHTML = ''
+    document.querySelectorAll('.show-option').forEach(
+      (e, i) => e.className = i === screen ? 'show-option' : 'show-option'
+    )
+    /*
+     if (oldScreen === screen) {
+    document.querySelector('#showcase-container').innerHTML = ''
+    document.querySelectorAll('.show-option').forEach(
+      (e, i) => e.className = 'show-option'
+    )
+     */
+    document.querySelector('#showcase-container').style.display = 'none'
+  } else {
+    document.querySelector('#showcase-container').innerHTML = document.querySelector(`#screen-${screen}`).innerHTML
+    document.querySelectorAll('.show-option').forEach(
+      (e, i) => e.className = i === screen ? 'show-option active' : 'show-option'
+    )
+    document.querySelector('#showcase-container').style.display = 'block'
+  }
+
+  oldScreen = screen
+}
+
+function chooseCourse(course) {
+  document.querySelectorAll('input[type=radio]')[course].checked = true
+  document.getElementById('myModal').style.display = "block";
+}
+
+window.onload = () => {
+  let modal = document.getElementById('myModal');
+  let btn = document.getElementById("myBtn");
+  let span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = () => {
+    window.scrollTo(0, 0);
+    modal.style.display = "block";
+  }
+
+  span.onclick = () => {
+    modal.style.display = "none";
+  }
+
+  window.onclick = (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  document.getElementById('cpf').addEventListener('keyup', (e) => {
+    const cpf = e.target.value;
+    const cpfSomenteNumeros = cpf.replace(/\D/g, '');
+
+    if (cpfSomenteNumeros.length === 11 && validaCPF(cpf)) {
+      e.target.value = formataCPF(cpf);
+    } else if (cpf.length === 14 && !validaCPF(cpf)) {
+      alert('CPF inválido!');
+      e.target.value = '';
+    }
+  });
+}
