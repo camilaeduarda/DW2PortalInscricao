f7bb7c2b6fcc6efbd958b672d29668efcd1230be
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2023-04-14 22:41:40 -0300

    Adaga de Cristal Apple Pie (0.1.0)
    Versão Do Projeto: Adaga de Cristal Apple Pie (0.1.0)
    
     - Animation of item effects, button functions and a touch of design!

---
+++ DW2 - Candy World/Apple Pie (0.1.0)/js/utils.js
@@ -0,0 +1,46 @@
+
+
+function validaCPF(cpf) {
+  cpf = cpf.replace(/\D/g, '');
+
+  if (cpf.length !== 11) {
+    return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+
+  let resto = 11 - (soma % 11);
+  if (resto === 10 || resto === 11) {
+    resto = 0;
+  }
+  if (resto !== parseInt(cpf.charAt(9))) {
+    return false;
+  }
+
+  soma = 0;
+  for (let i = 0; i < 10; i++) {
+    soma += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+
+  resto = 11 - (soma % 11);
+  if (resto === 10 || resto === 11) {
+    resto = 0;
+  }
+
+  return resto === parseInt(cpf.charAt(10));
+
+  /*
+  if (resto !== parseInt(cpf.charAt(10))) {
+    return false;
+  }
+   */
+
+}
+
+function formataCPF(cpf) {
+  const numeros = cpf.replace(/\D/g, '');
+  return numeros.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+}
