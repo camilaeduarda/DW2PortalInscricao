eef262df8704588d2d51d10c64a193b90bd08488
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2023-04-14 22:46:22 -0300

    Adaga de Cristal Apple Pie (0.1.0)
    Versão Do Projeto: Adaga de Cristal Apple Pie (0.1.0)
    
     - Animation of item effects, button functions and a touch of design!

---
+++ DW2 - Candy World/Apple Pie (0.1.0)/media/Apple Pie (0.1.0).go
@@ -0,0 +1,25 @@
+/*
+
+@Author: Dani;
+
+Files that will be found in the correct place, 
+to complete this project, 
+and also in order not to burden the GitHub repository too much!
+
+DW2PortalInscricao/media/
+
+itens:
+
+boneca.png
+
+eletro.jpeg
+
+ifmslogo.png
+
+info.jpeg
+
+qr.png
+
+sticky.png
+
+*/
