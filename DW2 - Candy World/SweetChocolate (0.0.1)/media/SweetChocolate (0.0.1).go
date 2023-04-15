feb28f83a8353a1fd09395563ff6e412011cc8e5
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2023-04-14 22:33:13 -0300

    Sussurro Dos Primeiros Dias SweetChocolate (0.0.1)
    Versão Do Projeto: Sussurro Dos Primeiros Dias Sweet Chocolate (0.0.1)
    
     - Bug fixes, errors and code optimization!

---
+++ DW2 - Candy World/SweetChocolate (0.0.1)/media/SweetChocolate (0.0.1).go
@@ -0,0 +1,23 @@
+/*
+
+Files that will be found in the correct place, 
+to complete this project, 
+and also in order not to burden the GitHub repository too much!
+
+DW2PortalInscricao/media/
+
+itens:
+
+boneca.png
+
+eletro.jpeg
+
+ifmslogo.png
+
+info.jpeg
+
+qr.png
+
+sticky.png
+
+*/
